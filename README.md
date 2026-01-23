7c6b10761beee3c69bda0d2f1f1f7e4202c45039
OlavoCC <olavochimello3@gmail.com>
2026-01-23 04:57:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Py-Discord-bot-using-cogs-Model
---
+++ cogs/exemplo.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class Exemplo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name='ping')
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+
+async def setup(bot):
+    await bot.add_cog(Exemplo(bot))
